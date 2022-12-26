96f297f4a1921213c300477851322252ba34104b
MVTT <ryabtzun2011@yandex.ru>
2022-12-26 14:17:49 +0300

    feat: create site mesto

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import '../index.html';
+import './index.css';
+
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
+const nameInput = document.querySelector('.popup__name-input');
+const statusInput = document.querySelector('.popup__status-input');
+const nameProfile = document.querySelector('.profile__name');
+const statusProfile = document.querySelector('.profile__status');
+
+nameInput.value = nameProfile.textContent;
+statusInput.value = statusProfile.textContent;
+
+editButton.addEventListener('click', () => {
+  popup.classList.add('popup_opened');
+  setTimeout(() => {
+    document.querySelector('.popup_opened').style.opacity = 1;
+  }, 1);
+});
+
+closeButton.addEventListener('click', () => {
+  document.querySelector('.popup_opened').style.opacity = 0;
+  setTimeout(() => {
+    popup.classList.remove('popup_opened');
+  }, 300);
+  nameInput.value = nameProfile.textContent;
+  statusInput.value = statusProfile.textContent;
+});
+
+popup.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  statusProfile.textContent = statusInput.value;
+  popup.classList.remove('popup_opened');
+});
